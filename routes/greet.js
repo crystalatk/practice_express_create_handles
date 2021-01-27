abbe5d8fd056a967abaf965333a5324f83f0fe90
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-01-27 12:53:08 -0500

    Small text edits

--- routes/greet.js
+++ routes/greet.js
@@ -3,12 +3,14 @@ const express = require('express'),
     router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send(`<h1>You are in the greet section. Please type a name at the end of the address for a greeting!</h1>`);
+    res.send(`<h1>You are in the greet section. Please type a '/name' at the end of the address for a greeting!</h1>`);
 });
 
 router.get('/:handle', (req, res) =>{
     const { handle } = req.params;
-    res.send(`<h1>${handle} is here!<br><br>Welcome to the page!</h1>`);
+    const capitalizeName = strToCapital => `${strToCapital.charAt(0).toUpperCase()}${strToCapital.slice(1)}`;
+    const name = capitalizeName(handle)
+    res.send(`<h1>${name} is here!<br><br>Welcome to the page, ${name}!</h1>`);
 })
 
 module.exports = router;
